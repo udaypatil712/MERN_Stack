0b5d9259378adf06f390264fe5b3acaa7c9edf8c
udaypatil712 <udaypatil712@users.noreply.github.com>
2025-11-03 21:30:16 +0530

    CSS Responsive

---
+++ Lecture 05-Nested Arrays & Array Questions/NestedArrays/Pattern1.js
@@ -0,0 +1,24 @@
+let n=5;
+
+// let t = String.fromCharCode(65);
+// console.log(t)
+
+// for(let i=0;i<n;i++){
+//     let str="";
+//     for(let j=0;j<=i;j++){
+//         let t =String.fromCharCode(65+i+j);
+//         str+=t;
+       
+//     }
+//      console.log(str);
+// }
+
+// for(let i=1;i<=n;i++){
+//     let str ="";
+//     for(let j=1;j<=i;j++){
+//         str+=i+j-1;
+        
+//     }
+//     console.log(str);
+// }
+
